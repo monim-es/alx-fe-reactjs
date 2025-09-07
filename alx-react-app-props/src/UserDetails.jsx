9ec11d568c5644979292ce97bef96b9411eb14d5
momo <monimsrw@outlook.fr>
2025-09-08 00:52:17 +0100

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/UserDetails.jsx
@@ -0,0 +1,15 @@
+import { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserDetails() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', maxWidth: '300px' }}>
+      <p><strong>Name:</strong> {userData.name}</p>
+      <p><strong>Email:</strong> {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
