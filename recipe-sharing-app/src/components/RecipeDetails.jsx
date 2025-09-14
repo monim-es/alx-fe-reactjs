398b7ef308e409fa2c644e4e61011cfe7f1744d4
momo <monimsrw@outlook.fr>
2025-09-14 22:21:05 +0100

    zustland task 0 and 1

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams, Link } from 'react-router-dom'
-import { useRecipeStore } from '../store/recipeStore'
+import { useRecipeStore } from './recipeStore'
 import EditRecipeForm from './EditRecipeForm'
 import DeleteRecipeButton from './DeleteRecipeButton'
 
